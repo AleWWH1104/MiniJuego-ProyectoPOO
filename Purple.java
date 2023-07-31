329cbae76a7231cb97880cb15b06b18dc1ae2644
David Dominguez <dominguezdavid396@gmail.com>
2023-07-30 19:29:53 -0600

    Implementacion de choques con los enemigos y obstaculos para perder

--- Purple.java
+++ Purple.java
@@ -19,11 +19,12 @@ public class Purple extends Actor
     public void act()
     {
         checkKeyPress();
+        validarColisionPerder();
     }
     
      public void checkKeyPress() {
          
-          int velocidad = 5; // Ajusta la velocidad de movimiento según lo desees
+          int velocidad = 4; // Ajusta la velocidad de movimiento según lo desees
 
     if (Greenfoot.isKeyDown("up")) {
         setLocation(getX(), getY() - velocidad);
@@ -38,4 +39,14 @@ public class Purple extends Actor
         setLocation(getX() + velocidad, getY());
     }
         }
+    private void resetGame()
+    {
+        // Reinicia todo el escenario aquí
+        Greenfoot.setWorld(new MyWorld());
+    }
+    public void validarColisionPerder(){
+        if (isTouching(snake.class) || isTouching(cohete.class) || isTouching(agua.class)) {
+            resetGame(); 
+        }
+    }
 }
